cb034a8dc805ac2e460da12ee855c8125267af63
jmjusmin <jmjusmin1412@gmail.com>
2022-10-30 18:11:30 -0500

    Set up firebase and clean up code to start

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC17acCDdgON6PPXT8uYtVFhXZT8_4dwcM",
+  authDomain: "swapit-9763e.firebaseapp.com",
+  projectId: "swapit-9763e",
+  storageBucket: "swapit-9763e.appspot.com",
+  messagingSenderId: "475991489514",
+  appId: "1:475991489514:web:6ce85f1e411ca0f583d093",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
