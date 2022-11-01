069bfc3b90e9ae0767743f58d57761f444c7c365
jmjusmin <jmjusmin1412@gmail.com>
2022-11-01 14:42:05 -0500

    Implement google OAuth

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,51 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import googleIcon from "../assets/svg/googleIcon.svg";
+
+function OAuth() {
+  //init
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      //create provider
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      //get a user from google the compare it to the document's reference
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      //check if user doesn't exist in db
+      if (!docSnap.exists()) {
+        //create user in db
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      //after sign-in/sign-up, then navigate to home
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  };
+
+  return (
+    <div className="socialLogin">
+      <p>Sign {location.pathname === "/sign-up" ? "up" : "in"} with </p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img src={googleIcon} alt="google" className="socialIconImg" />
+      </button>
+    </div>
+  );
+}
+
+export default OAuth;
