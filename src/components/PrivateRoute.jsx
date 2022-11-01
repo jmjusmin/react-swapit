928c6a2206aa8a2003a2644e77e1fde742ad1100
jmjusmin <jmjusmin1412@gmail.com>
2022-11-01 00:08:06 -0500

    Fix memory leak warning and add spinner components

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
 const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
 
   //if checkingStatus === true
   if (checkingStatus) {
-    return <h3>Loading</h3>;
+    return <Spinner />;
   }
 
   //else render the following
