8fb4880cd4e41a719f2ac0b49bbea518aeef1b30
jmjusmin <jmjusmin1412@gmail.com>
2022-11-03 15:09:31 -0700

    Set up an onSubmit function in CreateListing and enable geolocation from google API

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
 
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -59,8 +60,54 @@ function CreateListing() {
     };
   }, [isMounted]);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+
+    setLoading(true);
+
+    //check the discountPrice
+    if (discountPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less than regular price.");
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Max 6 images.");
+      return;
+    }
+
+    //set up lat, lng and address
+    let geolocation = {};
+    let location;
+    const mapAPIKey = process.env.REACT_APP_MAP_API_KEY;
+
+    if (geolocationEnabled) {
+      const res = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${mapAPIKey}`
+      );
+      const data = await res.json();
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0]?.formatted_address;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address.");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+    }
+
+    setLoading(false);
   };
 
   const onMutate = (e) => {
